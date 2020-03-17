4b1499e8eb7f084e3b931aeb5caf5215c018ef81
Junior Palomino <y.palomino@pucp.pe>
2020-03-17 17:05:19 -0500

    env-vars.sh modified

--- env-vars.sh
+++ env-vars.sh
@@ -1,15 +1,12 @@
 ### Authentication details
-### export TF_VAR_tenancy_ocid=ocid1.tenancy.oc1..aaaaaaaatbtjah3ht6up5waoi52wiyde5gdxbdvofjzok4ruo6k4tvlnl4ia
-### export TF_VAR_user_ocid=ocid1.user.oc1..aaaaaaaadzczsphbzlvsqymx6kbllrfybp6wpkx6hubfbxwyfjabg7ufm5zq
-export TF_VAR_fingerprint=52:fe:22:47:f0:a9:9e:15:2d:ca:36:b6:da:ad:8f:fd
-export TF_VAR_private_key_path=./api_keys/private.pem
-export TF_VAR_private_key_password=private
+export TF_VAR_tenancy_ocid=ocid1.tenancy.oc1..aaaaaaaal7ryxbp2hgljainhn3xe67m3jec66exupxajvsjcd36y5sfot7kq
+### no se necesita TF_VAR_user_ocid, TF_VAR_fingerprint y TF_VAR_private_key_path
 
 ### Region
 export TF_VAR_region=us-ashburn-1
 
 ### Compartment
-export TF_VAR_compartment_ocid=ocid1.compartment.oc1..aaaaaaaalxo7zindcffskunoclqhfxnvmblgpjs4r7s7li7cvx2l3p2mfniq
+export TF_VAR_compartment_ocid=ocid1.compartment.oc1..aaaaaaaaf3iat7bckqiavo53ltmcbxqiuu5kfbu5ijbsgretwgxo6rczawpa
 
 ### Public/private keys used on the compute instance
 export TF_VAR_host_user_name=opc
