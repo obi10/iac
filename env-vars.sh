f6756268a4495768163f21c50135de0171fa5de3
Junior Palomino <y.palomino@pucp.pe>
2020-03-17 15:49:44 -0500

    api_keys added

--- env-vars.sh
+++ env-vars.sh
@@ -1,8 +1,8 @@
 ### Authentication details
-export TF_VAR_tenancy_ocid=ocid1.tenancy.oc1..aaaaaaaatbtjah3ht6up5waoi52wiyde5gdxbdvofjzok4ruo6k4tvlnl4ia
-export TF_VAR_user_ocid=ocid1.user.oc1..aaaaaaaadzczsphbzlvsqymx6kbllrfybp6wpkx6hubfbxwyfjabg7ufm5zq
+### export TF_VAR_tenancy_ocid=ocid1.tenancy.oc1..aaaaaaaatbtjah3ht6up5waoi52wiyde5gdxbdvofjzok4ruo6k4tvlnl4ia
+### export TF_VAR_user_ocid=ocid1.user.oc1..aaaaaaaadzczsphbzlvsqymx6kbllrfybp6wpkx6hubfbxwyfjabg7ufm5zq
 export TF_VAR_fingerprint=52:fe:22:47:f0:a9:9e:15:2d:ca:36:b6:da:ad:8f:fd
-export TF_VAR_private_key_path=/home/junicode/.oci//private.pem
+export TF_VAR_private_key_path=./api_keys/private.pem
 export TF_VAR_private_key_password=private
 
 ### Region
