14ecd0b499131866da78529c922bf6e8a8e91845
Junior Palomino <y.palomino@pucp.pe>
2020-03-23 16:32:38 -0500

    zip file removed

--- env-vars.sh
+++ env-vars.sh
@@ -1,7 +1,7 @@
 ### Authentication details
 export TF_VAR_tenancy_ocid=ocid1.tenancy.oc1..aaaaaaaal7ryxbp2hgljainhn3xe67m3jec66exupxajvsjcd36y5sfot7kq
 ### no se necesita TF_VAR_user_ocid, TF_VAR_fingerprint y TF_VAR_private_key_path (si se usa Cloud Shell)
-### export TF_VAR_user_ocid=ocid1.user.oc1..aaaaaaaadzczsphbzlvsqymx6kbllrfybp6wpkx6hubfbxwyfjabg7ufm5zq
+### export TF_VAR_user_ocid=ocid1.user.oc1..aaaaaaaas2ol4ddk6yzmhabecxetak6cnqejwc7whhb7r567iiqxe7nvmpza
 ### export TF_VAR_fingerprint=52:fe:22:47:f0:a9:9e:15:2d:ca:36:b6:da:ad:8f:fd
 ### export TF_VAR_private_key_path=./user_keys/private.pem
 ### export TF_VAR_private_key_password=private
